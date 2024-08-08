5414f9883984526e1a4bf67026465d0da4240eb0
Felipe Porto <fe2017li@gmail.com>
2024-08-08 18:14:51 -0300

    8 a 9

--- blogpessoal75/src/App.tsx
+++ blogpessoal75/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './paginas/components/navBar/NavBar';
+import Footer from './paginas/components/footer/Footer';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Home/>
+    <Footer/>
     </>
 );
 }
